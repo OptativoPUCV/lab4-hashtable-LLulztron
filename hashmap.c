1ae99900f862a76d73c9242e3cb10c4c77ac7c73
LLulztron <163662385+LLulztron@users.noreply.github.com>
2024-04-24 20:57:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -142,7 +142,7 @@ Pair * nextMap(HashMap * map)
   {
     if(map -> buckets[map -> current] != NULL && map -> buckets[map -> current] -> key != NULL)
     {
-      return map -> buckets[map -> current];
+      return NULL;
     }
     map -> current = (map -> current + 1) % map -> capacity;
   }
